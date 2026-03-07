983533b6d32053a09557e47e591ef1c2f470ecc8
guimard11 <guimardsyvrain@gmail.com>
2026-03-07 12:40:21 -0500

    Creating notebook to process the datasets to build the semantic model

---
+++ NB_Silver_to_Gold_transformation.Notebook/notebook-content.py
@@ -0,0 +1,42 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {
+# META     "lakehouse": {
+# META       "default_lakehouse": "5010b322-daf9-4aa4-9bb2-5c28f6c1e634",
+# META       "default_lakehouse_name": "LH_mavenmart_bronze",
+# META       "default_lakehouse_workspace_id": "2869e8b2-b7be-4489-a03e-7704f314fce5",
+# META       "known_lakehouses": [
+# META         {
+# META           "id": "5010b322-daf9-4aa4-9bb2-5c28f6c1e634"
+# META         },
+# META         {
+# META           "id": "074869bc-7b4e-4e67-80ff-e81d792f0488"
+# META         }
+# META       ]
+# META     }
+# META   }
+# META }
+
+# MARKDOWN ********************
+
+# #### Processing raw data to be stored in Silver Lakehouse for further processing
+
+# CELL ********************
+
+# Welcome to your new notebook
+# Type here in the cell editor to add code!
+
+
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
